bf3977edfcc9283fe0acf1fd3f94e5e227508364
Daniil <visomnibus@yandex.ru>
2023-09-05 14:16:42 +0300

    and again trying fix cors :(

--- frontend/src/utils/AuthApi.js
+++ frontend/src/utils/AuthApi.js
@@ -43,7 +43,6 @@ class AuthApi {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
-        "Authorization": `Bearer ${token}`
       },
       credentials: 'include',
     })
